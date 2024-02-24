f3602c3b74ffeb03c400b94d77326b5220aa0f15
Paolar12 <145074525+Paolar12@users.noreply.github.com>
2024-02-24 17:56:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Stackfindover: Sign in</title>
+  <title>Registro de materias</title>
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
